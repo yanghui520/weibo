a9e00c695e4b9d5b40176a56937ed5ba28d1cead
vagrant <vagrant@homestead>
2019-02-26 13:33:28 +0000

    基础页面

---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title','dddd')
+@section('content')
+    <h1>cccc</h1>
+@stop
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title','蛀牙')
+@section('content')
+    <h1>ddd</h1>
+@stop
